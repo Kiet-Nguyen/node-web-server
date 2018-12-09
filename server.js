3d5b5e2b5421626b506d460a31a62a2c99dcf2a1
Kiet <bunbothomngon@gmail.com>
2018-12-09 15:20:45 +0200

    express middleware

--- server.js
+++ server.js
@@ -1,20 +1,58 @@
 const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
 
 const app = express();
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+// Middleware
+// Create a log file to keep track of server
+app.use((req, res, next) => {
+  const now = new Date().toString();
+  const log = `${now}: ${req.method} ${req.originalUrl}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', err => {
+    if (err) {
+      console.log('Unable to append to server.log');
+    }
+  });
+
+  next();
+});
+// Maintenance page
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', test => {
+  return test.toUpperCase();
+})
 
 app.get('/', (req, res) => {
   // res.send('<h1>Hello Express</h1>');
-  res.send({
-    name: 'Kiet',
-    likes: [
-      'games',
-      'movies',
-    ],
-  })
+  // res.send({
+  //   name: 'Kiet',
+  //   likes: [
+  //     'games',
+  //     'movies',
+  //   ],
+  // });
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to Node Web Server!'
+  });
 });
 
 app.get('/about', (req, res) => {
-  res.send('About page');
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
 });
 
 app.get('/bad', (req, res) => {
@@ -23,4 +61,6 @@ app.get('/bad', (req, res) => {
   });
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
